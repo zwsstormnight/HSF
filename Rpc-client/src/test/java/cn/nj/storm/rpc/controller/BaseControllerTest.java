f8ceec41c77ef2657ba7f60b2789e622d6a1cab3
zwsstormnight <zwsstormnight@163.com>
2018-02-06 18:26:08 +0800

    NIO client

---
+++ Rpc-client/src/test/java/cn/nj/storm/rpc/controller/BaseControllerTest.java
@@ -0,0 +1,27 @@
+package cn.nj.storm.rpc.controller;
+
+import cn.nj.storm.App;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * <一句话功能简述>
+ * <功能详细描述>
+ *
+ * @author zhengweishun
+ * @version [版本号, 2018/2/6]
+ * @see [相关类/方法]
+ * @since [产品/模块版本]
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = App.class)
+public class BaseControllerTest
+{
+    @Test
+    public void tt()
+    {
+        System.out.println("-----");
+    }
+}
---
+++ Rpc-client/src/test/java/cn/nj/storm/rpc/service/nio/impl/NioSimpleServiceTest.java
@@ -0,0 +1,37 @@
+package cn.nj.storm.rpc.service.nio.impl;
+
+import cn.nj.storm.App;
+import cn.nj.storm.rpc.service.nio.NioSimpleService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * <一句话功能简述>
+ * <功能详细描述>
+ *
+ * @author zhengweishun
+ * @version [版本号, 2018/2/6]
+ * @see [相关类/方法]
+ * @since [产品/模块版本]
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = App.class)
+public class NioSimpleServiceTest
+{
+    @Autowired
+    private NioSimpleService nioSimpleService;
+
+    @Test
+    public void start()
+        throws Exception
+    {
+        String result = nioSimpleService.start();
+        System.out.println(result);
+    }
+    
+}
