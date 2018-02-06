222f35214c5169861189a273c2483950592ac692
zwsstormnight <zwsstormnight@163.com>
2018-02-06 18:25:26 +0800

    NIO client

--- Rpc-client/src/main/java/cn/nj/storm/App.java
+++ Rpc-client/src/main/java/cn/nj/storm/App.java
@@ -1,13 +1,32 @@
 package cn.nj.storm;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App
 {
-    public static void main( String[] args )
+    private static final Logger logger = LoggerFactory.getLogger("interface");
+
+    public static void main(String[] args)
+            throws Exception
     {
-        System.out.println( "Hello World!" );
+        //spring日志桥接
+        SLF4JBridgeHandler.install();
+        //启动Spring Boot项目的入口
+        logger.info("Rpc-client 程序启动");
+        SpringApplication.run(App.class, args);
+        logger.info("Rpc-client 程序已启动");
+        synchronized (App.class)
+        {
+            while (true)
+            {
+                System.out.println("........");
+                App.class.wait();
+            }
+        }
     }
 }
