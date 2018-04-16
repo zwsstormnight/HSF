0cea6a7a56ada5207f86cdb88c1e7b3017fbcf35
weishun.zheng <Weishun.Zheng@connext.com.cn>
2018-04-16 16:07:33 +0800

    test 代码迁移

---
+++ shsf-applications/shsf-app-client/src/test/java/cn/nj/storm/shsf/app/client/nioDemos/service/NioSimpleService.java
@@ -0,0 +1,15 @@
+package cn.nj.storm.shsf.app.client.nioDemos.service;
+
+/**
+ * <NIO 基本接口>
+ * <功能详细描述>
+ *
+ * @author zhengweishun
+ * @version [版本号, 2018/2/6]
+ * @see [相关类/方法]
+ * @since [产品/模块版本]
+ */
+public interface NioSimpleService
+{
+    String start();
+}
