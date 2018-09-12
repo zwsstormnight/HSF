4f8f571ce3daa86274c4ff7bd4247fed3443c1e8
zhengweishun <zhengweishun@netposa.com>
2018-09-12 10:08:56 +0800

    20180912 依赖及拉起结构修整

--- shsf-core/src/main/java/cn/nj/storm/shsf/core/register/impl/ZkRegisterService.java
+++ shsf-core/src/main/java/cn/nj/storm/shsf/core/register/impl/ZkRegisterService.java
@@ -9,10 +9,8 @@ import org.apache.zookeeper.ZooDefs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
-import org.springframework.stereotype.Service;
 
 import java.util.Map;
-import java.util.Objects;
 
 /**
  * <zookeeper注册实现>
